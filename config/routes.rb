14ae315e838ec34e7c379a0bfa43304c52765e15
TamNDT259 <1312508@student.hcmus.edu.vn>
2016-10-11 11:06:34 +0700

    post status feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
- get "log_out" => "sessions#destroy", :as => "log_out"
-get "log_in" => "sessions#new", :as => "log_in"
-get "sign_up" => "users#new", :as => "sign_up"
-root :to => "users#new"
-resources :users
-resources :sessions
+  get 'posts/index'
+
+    get "log_out" => "sessions#destroy", :as => "log_out"
+    get "log_in" => "sessions#new", :as => "log_in"
+    get "sign_up" => "users#new", :as => "sign_up"
+    root :to => "sessions#new"
+    resources :users
+    resources :sessions
+    resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
